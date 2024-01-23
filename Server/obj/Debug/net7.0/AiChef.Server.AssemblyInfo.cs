3c4be82876d54d9750fe4ef3237c78868437fb34
iparker3964 <iparker3964@yahoo.com>
2024-01-23 13:02:02 -0600

    add docker support for .NET 7

--- Server/obj/Debug/net7.0/AiChef.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net7.0/AiChef.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AiChef.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+016b6d149338cc7deb52b5b2a026cc2d0cdb781d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AiChef.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AiChef.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
